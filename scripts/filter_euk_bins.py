6c4d295579c71a90ca1715e9456345f92c8f1a47
Paul Saary <saary@ebi.ac.uk>
2020-11-06 14:35:07 +0000

    Make filtering use multithread

--- scripts/filter_euk_bins.py
+++ scripts/filter_euk_bins.py
@@ -21,6 +21,7 @@ import argparse
 import subprocess
 import logging
 import tempfile
+from multiprocessing import Pool
 
 
 # backup fasta handler, so we can use readonly directories
@@ -58,26 +59,6 @@ def Fasta(path):
         # yield last one
         entry.seq = "".join(entry.seq)
         yield entry
-
-
-def concatenate_bins(bins, outf):
-    """simple function to merge all
-    bins into scaffolds file"""
-    with open(outf, "w") as outfile:
-        for path in bins:
-            logging.debug(f"Concatenating bin {path}")
-            fa = Fasta(path)
-            name = os.path.basename(path)
-            for seq in fa:
-                outfile.write(">{}_binsep_{}\n{}\n".format(name, seq.name, seq))
-
-
-def create_dir(d):
-    if not os.path.isdir(d):
-        try:
-            os.makedirs(d)
-        except OSError as e:
-            logging.warning(f"Could not create dir: {d}\n{e}")
 
 
 class EukRep:
@@ -129,22 +110,24 @@ class EukRep:
 
 class bin:
     def __init__(self, path, eukrep):
-        self.path = path
         self.e = eukrep
+        self.bname = os.path.basename(path)
+        self.path = os.path.abspath(path)
+
+    def __str__(self):
+        return "{} euks: {} bacs: {}".format(self.bname, self.table["euks"], self.table["bacs"])
 
     def stats(self):
         """read bin content and figure genomic composition"""
         logging.debug("Loading bin")
         fa_file = Fasta(self.path)
-        name = os.path.basename(self.path)
         stats = {"euks": 0, "bacs": 0, "NA": 0, "sum": 0}
         # loop and compute stats
         logging.debug("Make per bin stats")
         for seq in fa_file:
-            ename = "{}_binsep_{}".format(name, seq.name)
-            if ename in self.e.euks:
+            if seq.name in self.e.euks:
                 stats["euks"] += len(seq)
-            elif ename in self.e.bacs:
+            elif seq.name in self.e.bacs:
                 stats["bacs"] += len(seq)
             else:
                 stats["NA"] += len(seq)
@@ -182,7 +165,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--output", help="path for the output table", default="assignment.csv", type=str)
     parser.add_argument("bins", nargs="+", help="all bins to classify", type=str)
-    parser.add_argument("--tempdir", type=str, help="Will save temp files of the analysis here", default="tmp")
+    parser.add_argument(
+        "--threads", "-t", type=int, help="How many bins should be run in parallel (Default: 1)", default=1,
+    )
     parser.add_argument(
         "--minl",
         type=int,
@@ -194,29 +179,29 @@ if __name__ == "__main__":
         "--eukratio",
         type=float,
         help="This ratio of eukaryotic DNA to all DNA has to be found\
-                    at least (default: 0)",
+                    at least (default: 0, ignore)",
         default=0,
     )
     parser.add_argument(
         "--bacratio",
         type=float,
         help="discard bins with bacterial ratio of higher than\
-                    (default: 1)",
+                    (default: 1, ignore)",
         default=1,
     )
     parser.add_argument(
         "--minbp",
         type=float,
         help="Only keep bins with at least n bp of dna\
-                    (default: 100000)",
-        default=100000,
+                    (default: 8000000)",
+        default=8000000,
     )
     parser.add_argument(
         "--minbpeuks",
         type=float,
         help="Only keep bins with at least n bp of Eukaryotic dna\
-                    (default: 1000000)",
-        default=1000000,
+                    (default: 5000000)",
+        default=5000000,
     )
     parser.add_argument("--rerun", action="store_true", help="rerun even if output exists", default=False)
     parser.add_argument("--quiet", action="store_true", help="supress information", default=False)
@@ -232,38 +217,28 @@ if __name__ == "__main__":
         logLevel = logging.DEBUG
     logging.basicConfig(format="%(asctime)s %(message)s", datefmt="%m/%d/%Y %H:%M:%S: ", level=logLevel)
 
-    # creating tmp dir if not exists
-    with tempfile.TemporaryDirectory(prefix="filter_EukRep_") as tmp_dir:
-        logging.debug("Created tempdir: {}".format(tmp_dir))
-        # concatenate fastas as we need single file for EukRep
-        contigs = os.path.join(tmp_dir, "contigs.fna")
-        logging.info("Concatenating bins")
-        if args.rerun or not os.path.exists(contigs):
-            concatenate_bins(args.bins, contigs)
-        else:
-            logging.debug("Reusing concatenated contigs")
-
-        # launch EukRep
-        eukfile = os.path.join(tmp_dir, "euks.fna")
-        bacfile = os.path.join(tmp_dir, "bacs.fna")
-        eukrep_result = EukRep(contigs, eukfile, bacfile, minl=args.minl)
-
-        if args.rerun or not os.path.exists(eukfile):
-            logging.info("Running EukRep on concatenated contigs")
+    def evaluate_bin(path):
+        logging.info("Launch on {}".format(path))
+        with tempfile.TemporaryDirectory(prefix="filter_EukRep_") as tmp_dir:
+            logging.debug("Using tmp folder: {}".format(tmp_dir))
+            eukfile = os.path.join(tmp_dir, "euks.fna")
+            bacfile = os.path.join(tmp_dir, "bacs.fna")
+            eukrep_result = EukRep(path, eukfile, bacfile, minl=args.minl)
             eukrep_result.run()
-        else:
-            logging.debug("Reusing EukRep run from before")
-            eukrep_result.read_result()
+            b = bin(path, eukrep_result)
+            b.stats()
+            b.decide(eukratio=args.eukratio, bacratio=args.bacratio, minbp=args.minbp, minbpeuks=args.minbpeuks)
+            return b
 
-        with open(args.output, "w") as outfile:
-            outfile.write("path,binname,passed,bp_eukaryote,bp_prokaryote,bp_unassigned,bp_sum\n")
-            for path in args.bins:
-                logging.info(f"Deciding on bin: {path}")
-                b = bin(path, eukrep_result)
-                b.stats()
-                b.decide(eukratio=args.eukratio, bacratio=args.bacratio, minbp=args.minbp, minbpeuks=args.minbpeuks)
-                bname = os.path.basename(path)
-                path = os.path.abspath(path)
-                outfile.write(
-                    f"{path},{bname},{b.keep},{b.table['euks']},{b.table['bacs']},{b.table['NA']},{b.table['sum']}\n"
-                )
+    # multithreading pool
+    pool = Pool(processes=args.threads)
+    results = pool.map(evaluate_bin, args.bins)
+    pool.close()
+    pool.join()
+
+    with open(args.output, "w") as outfile:
+        outfile.write("path,binname,passed,bp_eukaryote,bp_prokaryote,bp_unassigned,bp_sum\n")
+        for b in results:
+            outfile.write(
+                f"{b.path},{b.bname},{b.keep},{b.table['euks']},{b.table['bacs']},{b.table['NA']},{b.table['sum']}\n"
+            )
