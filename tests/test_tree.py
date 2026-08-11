736171f56c8c13170b39a6475d54c8d8c4782a4c
dezordi <zimmer@ebi.ac.uk>
2026-08-11 16:39:22 +0100

    add test tax_LCA

--- tests/test_tree.py
+++ tests/test_tree.py
@@ -1,10 +1,11 @@
 import unittest
 import os
-from eukcc.treehandler import tree_sets
+from eukcc.treehandler import tree_sets, tax_LCA
 
 TESTDATA_SCMG_GZ = os.path.join(os.path.dirname(__file__), "testfiles", "SCMG_TEST.csv.gz")
 TESTDATA_SCTREE = os.path.join(os.path.dirname(__file__), "testfiles", "tree_scmg.csv")
 TESTDATA_TAXINFO = os.path.join(os.path.dirname(__file__), "testfiles", "tree_taxinfo.csv")
+TESTDATA_TAX_LCA = os.path.join(os.path.dirname(__file__), "testfiles", "tax_lca_taxinfo.csv")
 
 
 class test_tree(unittest.TestCase):
@@ -21,3 +22,16 @@ class test_tree(unittest.TestCase):
             s, placement, TESTDATA_SCTREE, set_species=4, set_size=2, set_prevalence=80, taxinfo=TESTDATA_TAXINFO
         )
         self.assertEqual(t.marker_set.profiles, set(["Marker100", "Marker80"]))
+
+    def test_tax_LCA_partial_placements(self):
+        tree = (
+            "(((GCA_015473125.1,metaeuk_OY253681.1_29),(((GCA_006384855.1,metaeuk_OY253673.1_71),"
+            "metaeuk_OY253685.1_62),((((metaeuk_OY253682.1_41,GCA_004138255.1),metaeuk_OY253672.1_198),"
+            "((GCA_011766145.1,metaeuk_OY253676.1_95),(GCA_004335775.1,metaeuk_OY253680.1_173))),"
+            "((GCA_010646915.1,metaeuk_OY253673.1_210),((metaeuk_OY253668.1_317,GCA_002794665.1),"
+            "GCA_000690575.1))))),metaeuk_OY253675.1_91);"
+        )
+        lng = tax_LCA(tree, TESTDATA_TAX_LCA, placements=["metaeuk_OY253681.1_29"], add_protist_common=False)
+
+        # root, cellular organisms, Eukaryota, Viridiplantae, Chlorophyta, Chlorophyta incertae sedis, Pycnococcaceae
+        self.assertEqual(lng, ["1", "131567", "2759", "33090", "3041", "34155", "41878"])
